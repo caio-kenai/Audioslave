d242716ee4e1a9cd89760abeb00c91775e4b994f
Caio Oliveira Pacifico <caio.oliveira@playlist.com.br>
2026-09-24 12:17:41 -0300

    Build: Add version resources and manifests for the service and tests

---
+++ resources/resource.h
@@ -0,0 +1,10 @@
+#ifndef AUDIOSLAVE_RESOURCE_H
+#define AUDIOSLAVE_RESOURCE_H
+// Resource identifiers shared by the Audioslave executables (.rc and C++).
+
+#define IDI_APP         101
+#define IDI_APP_PAUSED  102
+
+#define IDR_PAYLOAD     200
+
+#endif
